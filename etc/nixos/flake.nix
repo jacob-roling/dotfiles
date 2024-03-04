7b35d9abea0c2fe46f7ce3f3d0c16aa370de3a4d
Jacob, Roling <jacob.roling@hey.com>
2024-03-04 17:26:39 +1030

    chore: savepoint

--- etc/nixos/flake.nix
+++ etc/nixos/flake.nix
@@ -4,10 +4,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
 
-    # home-manager = {
-    #   url = "github:nix-community/home-manager";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     hyprland = {
       url = "github:hyprwm/Hyprland";
@@ -31,7 +31,7 @@
 
         modules = [
           ./hosts/desktop/configuration.nix
-          # inputs.home-manager.nixosModules.default
+          inputs.home-manager.nixosModules.default
         ];
       };
 
