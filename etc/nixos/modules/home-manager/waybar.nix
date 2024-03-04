7674d0c33986ce4beef3775f3a16f1e90fc92ac8
Jacob, Roling <jacob.roling@hey.com>
2024-03-04 23:13:17 +1030

    chore: savepoint

--- etc/nixos/modules/home-manager/waybar.nix
+++ etc/nixos/modules/home-manager/waybar.nix
@@ -1,10 +1,10 @@
 { pkgs, inputs, config, ... }:
 {
   xdg.configFile."waybar/config.jsonc".source = ../../../../.config/waybar/config.jsonc;
+  xdg.configFile."waybar/style.css".source = ../../../../.config/waybar/style.css;
 
   programs.waybar = {
     enable = true;
     systemd.enable = true;
-    style = {};
   };
 }
