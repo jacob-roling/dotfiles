1e232a8d837d386cce12d9fddc43713fb49a4717
Jacob, Roling <jacob.roling@hey.com>
2024-03-05 18:29:13 +1030

    chore: savepoint

--- etc/nixos/modules/home-manager/nvim.nix
+++ etc/nixos/modules/home-manager/nvim.nix
@@ -1,10 +1,12 @@
-{ ... }:
+{ config, ... }:
 {
-  xdg.configFile."nvim" = {
-    source = ../../../../.config/nvim;
-    recursive = true;
-  };
+  # xdg.configFile."nvim" = {
+  #   source = ../../../../.config/nvim;
+  #   recursive = true;
+  # };
 
+  home.file.".config/nvim".source = config.lib.file.mkOutOfStoreSymlink ../../../../.config/nvim;
+ 
   programs.neovim = {
     enable = true;
     defaultEditor = true;
