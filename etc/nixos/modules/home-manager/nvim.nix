02ce5cf07dc39573f8d451141691d50633ad3e26
Jacob, Roling <jacob.roling@hey.com>
2024-03-05 21:11:31 +1030

    chore: savepoint

--- etc/nixos/modules/home-manager/nvim.nix
+++ etc/nixos/modules/home-manager/nvim.nix
@@ -10,7 +10,27 @@
   programs.nixvim = {
     enable = true;
     
-    colorschemes.base16.customColorScheme = config.colorScheme.palette;
+    colorschemes.base16 = {
+      enable = true;
+      customColorScheme = with config.colorScheme.palette; {
+        base00 = "#${base00}";
+        base01 = "#${base01}";
+        base02 = "#${base02}";
+        base03 = "#${base03}";
+        base04 = "#${base04}";
+        base05 = "#${base05}";
+        base06 = "#${base06}";
+        base07 = "#${base07}";
+        base08 = "#${base08}";
+        base09 = "#${base09}";
+        base0A = "#${base0A}";
+        base0B = "#${base0B}";
+        base0C = "#${base0C}";
+        base0D = "#${base0D}";
+        base0E = "#${base0E}";
+        base0F = "#${base0F}";
+      };
+    };
 
     plugins = {
       lsp = {
@@ -18,7 +38,11 @@
         servers = {
           tsserver.enable = true;
           lua-ls.enable = true;
-          rust-analyzer.enable = true;
+          rust-analyzer = {
+	    enable = true;
+	    installRustc = false;
+	    installCargo = false;
+	  };
           gopls.enable = true;
         };
       };
