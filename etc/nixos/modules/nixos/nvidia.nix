bea3619a2fe5305e3f1a419cb27b87e847386704
Jacob, Roling <jacob.roling@hey.com>
2024-03-12 20:44:07 +1030

    chore: savepoint

--- etc/nixos/modules/nixos/nvidia.nix
+++ etc/nixos/modules/nixos/nvidia.nix
@@ -25,10 +25,12 @@
 
   hardware.nvidia = {
     modesetting.enable = true;
+    powerManagement.enable = false;
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
+    # forceFullCompositionPipeline = true;
   };
 
   boot.kernelModules = [
