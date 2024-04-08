a0ecec31a2d86a999c8a381c82c9221a8bcf6bf4
Jacob, Roling <jacob.roling@hey.com>
2024-04-08 17:33:36 +0930

    chore: savepoint

--- etc/nixos/modules/nixos/nvidia.nix
+++ etc/nixos/modules/nixos/nvidia.nix
@@ -48,12 +48,12 @@
       };
     in config.boot.kernelPackages.nvidiaPackages.mkDriver {
       version = "550.67";
-      sha256_64bit = "sha256-5tylYmomCMa7KgRs/LfBrzOLnpYafdkKwJu4oSb/AC4=";
+      sha256_64bit = "sha256-mSAaCccc/w/QJh6w8Mva0oLrqB+cOSO1YMz1Se/32uI=";
       sha256_aarch64 = "sha256-i6jZYUV6JBvN+Rt21v4vNstHPIu9sC+2ZQpiLOLoWzM=";
       openSha256 = "sha256-/Hxod/LQ4CGZN1B1GRpgE/xgoYlkPpMh+n8L7tmxwjs=";
       settingsSha256 = "sha256-QKN/gLGlT+/hAdYKlkIjZTgvubzQTt4/ki5Y+2Zj3pk=";
       persistencedSha256 = "sha256-FRMqY5uAJzq3o+YdM2Mdjj8Df6/cuUUAnh52Ne4koME=";
-      
+
       patches = [ rcu_patch ];
     };
   };
