9477f6b71532bd0c04135138f4374597dae1434f
Jacob, Roling <jacob.roling@hey.com>
2024-04-08 17:22:00 +0930

    chore: savepoint

--- etc/nixos/modules/nixos/nvidia.nix
+++ etc/nixos/modules/nixos/nvidia.nix
@@ -40,19 +40,14 @@
 
     # package = config.boot.kernelPackages.nvidiaPackages.stable;
     
-    package = let rcu_patch = pkgs.fetchpatch {
-      url = "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
-      hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
-    }; in config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "550.40.07";
-      sha256_64bit = "sha256-KYk2xye37v7ZW7h+uNJM/u8fNf7KyGTZjiaU03dJpK0=";
-      sha256_aarch64 = "sha256-AV7KgRXYaQGBFl7zuRcfnTGr8rS5n13nGUIe3mJTXb4=";
-      openSha256 = "sha256-mRUTEWVsbjq+psVe+kAT6MjyZuLkG2yRDxCMvDJRL1I=";
-      settingsSha256 = "sha256-c30AQa4g4a1EHmaEu1yc05oqY01y+IusbBuq+P6rMCs=";
-      persistencedSha256 = "sha256-11tLSY8uUIl4X/roNnxf5yS2PQvHvoNjnd2CB67e870=";
-
-      patches = [ rcu_patch ];
-    };
+    package = config.boot.kernelPackages.nvidiaPackages.beta.overrideAttrs (old: let
+      version = "550.67";
+    in {
+      src = pkgs.fetchurl {
+        url = "https://download.nvidia.com/XFree86/Linux-x86_64/${version}/NVIDIA-Linux-x86_64-${version}.run";
+        sha256 = "";
+      };
+    });
   };
 
   boot.kernelModules = [
