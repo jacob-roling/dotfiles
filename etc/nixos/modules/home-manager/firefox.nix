cc4f88a6bbd69041d55b341a75a54fec7fb2003b
Jacob, Roling <jacob.roling@hey.com>
2024-04-14 14:27:37 +0930

    chore: savepoint

--- etc/nixos/modules/home-manager/firefox.nix
+++ etc/nixos/modules/home-manager/firefox.nix
@@ -6,12 +6,19 @@
       jacob = {
         id = 0;
         isDefault = true;
-        extensions = with config.nur.repos.rycee.firefox-addons; [
-          bitwarden
-          ublock-origin
-          wappalyzer
-          joplin-web-clipper
-        ];
+        # extensions = with config.nur.repos.rycee.firefox-addons; [
+        #   bitwarden
+        #   ublock-origin
+        #   wappalyzer
+        # ];
+      };
+    };
+    policies = {
+      ExtensionSettings = rec {
+        "uBlock0@raymondhill.net": {
+          "installation_mode": "force_installed",
+          "install_url": "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi"
+        };
       };
     };
   };
