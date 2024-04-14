9af9230751c52c1003bda2d666ec4bd574de387c
Jacob, Roling <jacob.roling@hey.com>
2024-04-14 14:38:07 +0930

    chore: savepoint

--- etc/nixos/modules/home-manager/firefox.nix
+++ etc/nixos/modules/home-manager/firefox.nix
@@ -22,6 +22,10 @@
           "installation_mode" = "force_installed";
           "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
         };
+        "Wappalyzer@Wappalyzer" = {
+          "installation_mode" = "force_installed";
+          "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/wappalyzer/latest.xpi";
+        };
       };
     };
   };
