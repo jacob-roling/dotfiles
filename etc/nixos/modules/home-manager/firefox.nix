e12187db3ade18676620ba396130aba97a310734
Jacob, Roling <jacob.roling@hey.com>
2024-04-14 14:27:57 +0930

    chore: savepoint

--- etc/nixos/modules/home-manager/firefox.nix
+++ etc/nixos/modules/home-manager/firefox.nix
@@ -15,9 +15,9 @@
     };
     policies = {
       ExtensionSettings = rec {
-        "uBlock0@raymondhill.net": {
-          "installation_mode": "force_installed",
-          "install_url": "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi"
+        "uBlock0@raymondhill.net" = {
+          "installation_mode" = "force_installed",
+          "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi"
         };
       };
     };
