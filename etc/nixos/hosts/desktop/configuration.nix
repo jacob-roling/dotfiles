d5979535daf68d1fb19f1d1752d7bcfcd849abf5
Jacob <=>
2024-04-22 21:16:51 +0930

    chore: savepoint

--- etc/nixos/hosts/desktop/configuration.nix
+++ etc/nixos/hosts/desktop/configuration.nix
@@ -18,13 +18,6 @@ in
     ../../modules/nixos/git.nix
   ];
 
-  programs.nh = {
-    enable = true;
-    clean.enable = true;
-    clean.extraArgs = "--keep-since 4d --keep 3";
-    flake = "/home/user/my-nixos-config";
-  };
-
   home-manager = {
     extraSpecialArgs = { inherit inputs; };
     users = {
