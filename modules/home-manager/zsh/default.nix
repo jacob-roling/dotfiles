6a05cbb3747429beaf23e1b431fbde644de666d6
Jacob <=>
2024-04-23 00:29:16 +0930

    chore: savepoint

--- modules/home-manager/zsh/default.nix
+++ modules/home-manager/zsh/default.nix
@@ -1,4 +1,12 @@
 { config, pkgs, input, ... }:
 {
+  environment.systemPackages = with pkgs; [
+    zoxide
+    lsd
+    ripgrep
+    bottom
+    starship
+  ];
+
   xdg.configFile."../.zshrc".source = ./.zshrc;
 }
