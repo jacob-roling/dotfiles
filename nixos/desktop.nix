0cdffa5efb357f701ad723ee167fcf220199902e
Jacob <=>
2024-04-22 21:29:50 +0930

    chore: savepoint

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -9,7 +9,7 @@
 }: {
   imports = [
     ./common.nix
-    ./hardware-configurations/desktop.nix
+    ../hardware-configurations/desktop.nix
     # inputs.nixos-hardware.nixosModules.common-cpu-amd
     # Custom NixOS Modules
     # outputs.nixosModules.example
