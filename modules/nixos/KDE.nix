2b4a41a82346d8310ad44570b51e9829a349b5c3
Jacob <=>
2024-04-23 14:08:31 +0930

    chore: savepoint

--- modules/nixos/KDE.nix
+++ modules/nixos/KDE.nix
@@ -3,40 +3,39 @@
   pkgs,
   ...
 }: {
-  services.xserver = {
-    # enable = true;
-    # libinput.enable = true;
+  # services.xserver = {
+  #   enable = true;
+  #   libinput.enable = true;
     
-    # displayManager = {
-    #   sddm = {
-    #     enable = true;
-    #     wayland.enable = true;
-    #   };
-      
-    #   defaultSession = "plasma";
-    # };
+  #   displayManager = {
+  #     sddm = {
+  #       enable = true;
+  #     };
+  #   };
 
-    # desktopManager.plasma5.enable = true;
+  #   desktopManager.plasma5.enable = true;
 
-    excludePackages = with pkgs; [ xterm ];
-  };
+  #   excludePackages = with pkgs; [ xterm ];
+  # };
 
-  services.displayManager = {
-    sddm = {
-      enable = true;
-      wayland.enable = true;
-    };  
-    
-    defaultSession = "plasma";
-  };
-
-  services.desktopManager.plasma6.enable = true;
-  
   # environment.plasma5.excludePackages = with pkgs.libsForQt5; [
   #   plasma-browser-integration
   #   konsole
   #   oxygen
   # ];
+
+  services = {
+    desktopManager.plasma6.enable = true;
+
+    displayManager = {
+      sddm = {
+        enable = true;
+        wayland.enable = true;
+      };
+
+      defaultSession = "plasma";
+    };
+  };
 
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
     plasma-browser-integration
