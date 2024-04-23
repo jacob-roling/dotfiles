11ff7b0855dc1ae97ca412caca2afe8e08a20b89
Jacob <=>
2024-04-23 13:33:37 +0930

    chore: savepoint

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
 
     # Home Manager
     home-manager = {
-      url = "github:nix-community/home-manager/release-unstable";
+      url = "github:nix-community/home-manager/master";
       # You can change this to "nixpkgs-unstable" to use latest home-manager.
       # Then you also have to change nixpkgs to nixpkgs-unstable in homeConfigurations below.
       inputs.nixpkgs.follows = "nixpkgs";
