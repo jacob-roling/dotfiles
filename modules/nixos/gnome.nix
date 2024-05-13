6161d55830baabf9ad22d25b05531b367b6c8d80
Jacob <=>
2024-05-13 23:51:14 +0930

    chore: savepoint

--- modules/nixos/gnome.nix
+++ modules/nixos/gnome.nix
@@ -11,11 +11,11 @@
   environment.gnome.excludePackages = (with pkgs; [
     gnome-photos
     gnome-tour
+    gedit
   ]) ++ (with pkgs.gnome; [
     cheese # webcam tool
     gnome-music
     gnome-terminal
-    gedit # text editor
     epiphany # web browser
     geary # email reader
     evince # document viewer
