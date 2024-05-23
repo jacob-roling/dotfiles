aaa1203898e48a5512b2ba00e023f11da833f861
Jacob <=>
2024-05-23 23:38:27 +0930

    chore: savepoint

--- nixos/packages/gaming.nix
+++ nixos/packages/gaming.nix
@@ -25,4 +25,14 @@
   # XBox Accessories
   hardware.xone.enable = true;
   hardware.xpadneo.enable = true;
+  boot.extraModulePackages = [
+    (config.boot.kernelPackages.callPackage ./xpad.nix {})
+  ];
+  udev.extraRules = ''
+ACTION=="add", \
+  ATTRS{idVendor}=="2dc8", \
+  ATTRS{idProduct}=="3106", \
+  RUN+="${pkgs.kmod}/bin/modprobe xpad", \
+  RUN+="${pkgs.bash}/bin/sh -c 'echo 2dc8 3106 > /sys/bus/usb/drivers/xpad/new_id'"
+'';
 }
