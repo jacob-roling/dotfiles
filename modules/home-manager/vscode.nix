1b2e90aee1dd05baff27808fb92bfd3b2235ad68
Jacob <=>
2024-06-02 12:20:51 +0930

    chore: savepoint

--- modules/home-manager/vscode.nix
+++ modules/home-manager/vscode.nix
@@ -5,7 +5,7 @@ in
 {
   programs.vscode = {
     enable = true;
-    package = pkgs.vscodium;
+    package = pkgs.vscodium.fhs;
     userSettings = {
       "window.titleBarStyle" = "custom";
       "editor.formatOnPaste" = true;
