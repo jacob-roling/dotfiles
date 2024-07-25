26836457d7d3cf4dff5e909d9f2e59c1874830ff
Jacob <jacob.roling@hey.com>
2024-07-25 16:08:51 +0930

    chore: savepoint

--- modules/home-manager/vscode.nix
+++ modules/home-manager/vscode.nix
@@ -54,37 +54,37 @@ in
       };
       "sqltools.useNodeRuntime" = true;
     };
-    # extensions = with extensions.vscode-marketplace; [
-    #   a-h.templ
-    #   bradlc.vscode-tailwindcss
-    #   mtxr.sqltools
-    #   mtxr.sqltools-driver-sqlite
-    #   thebearingedge.vscode-sql-lit
-    #   adonisjs.vscode-edge
-    #   ziglang.vscode-zig
-    #   gleam.gleam
-    #   otovo-oss.htmx-tags
-    #   esbenp.prettier-vscode
-    #   golang.go
-    #   pkief.material-icon-theme
-    #   redhat.vscode-yaml
-    #   yzhang.markdown-all-in-one
-    #   dotenv.dotenv-vscode
-    #   christian-kohler.path-intellisense
-    #   formulahendry.auto-rename-tag
-    #   wix.vscode-import-cost
-    #   astro-build.astro-vscode
-    #   tamasfe.even-better-toml
-    #   attilabuti.mustache-syntax-vscode
-    #   polymeilex.wgsl
-    #   ritwickdey.liveserver
-    #   slevesque.shader
-    #   geequlim.godot-tools
-    #   bbenoist.nix
-    #   mkhl.direnv
-    # ] ++ (with extensions.open-vsx-release; [
-    #   rust-lang.rust-analyzer
-    # ]);
+    extensions = with extensions.vscode-marketplace; [
+      a-h.templ
+      bradlc.vscode-tailwindcss
+      mtxr.sqltools
+      mtxr.sqltools-driver-sqlite
+      thebearingedge.vscode-sql-lit
+      adonisjs.vscode-edge
+      ziglang.vscode-zig
+      gleam.gleam
+      otovo-oss.htmx-tags
+      esbenp.prettier-vscode
+      golang.go
+      pkief.material-icon-theme
+      redhat.vscode-yaml
+      yzhang.markdown-all-in-one
+      dotenv.dotenv-vscode
+      christian-kohler.path-intellisense
+      formulahendry.auto-rename-tag
+      wix.vscode-import-cost
+      astro-build.astro-vscode
+      tamasfe.even-better-toml
+      attilabuti.mustache-syntax-vscode
+      polymeilex.wgsl
+      ritwickdey.liveserver
+      slevesque.shader
+      geequlim.godot-tools
+      bbenoist.nix
+      mkhl.direnv
+    ] ++ (with extensions.open-vsx-release; [
+      rust-lang.rust-analyzer
+    ]);
   };
 }
 
