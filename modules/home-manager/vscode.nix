0f083e7f9765b0b1541e1f0d8cde89e72bbb4619
Jacob <jacob.roling@hey.com>
2024-08-05 10:11:47 +0930

    chore: savepoint

--- modules/home-manager/vscode.nix
+++ modules/home-manager/vscode.nix
@@ -56,6 +56,7 @@ in
     };
     extensions = with extensions.vscode-marketplace; [
       adonisjs.vscode-edge
+      jock.svg
       a-h.templ
       bradlc.vscode-tailwindcss
       mtxr.sqltools
