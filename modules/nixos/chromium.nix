c0af01721898c8f538973fbc34d8a655e8b2c583
Jacob <jacob.roling@hey.com>
2024-08-07 13:12:21 +0930

    chore: savepoint

--- modules/nixos/chromium.nix
+++ modules/nixos/chromium.nix
@@ -21,14 +21,14 @@
       { id = "jdbnofccmhefkmjbkkdkfiicjkgofkdh"; }
     ];
   };
-  extraOpts = {
-    "BrowserSignin" = 0;
-    "SyncDisabled" = true;
-    "PasswordManagerEnabled" = false;
-    "BuiltInDnsClientEnabled" = false;
-    "MetricsReportingEnabled" = true;
-    "SpellcheckEnabled" = true;
-    "SpellcheckLanguage" = ["en-AU"];
-    "CloudPrintSubmitEnabled" = false;
-  };
+  # extraOpts = {
+  #   "BrowserSignin" = 0;
+  #   "SyncDisabled" = true;
+  #   "PasswordManagerEnabled" = false;
+  #   "BuiltInDnsClientEnabled" = false;
+  #   "MetricsReportingEnabled" = true;
+  #   "SpellcheckEnabled" = true;
+  #   "SpellcheckLanguage" = ["en-AU"];
+  #   "CloudPrintSubmitEnabled" = false;
+  # };
 }
