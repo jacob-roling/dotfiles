dbed47434a8a13988eca12c1e9e6cc8b81fa09a6
Jacob <jacob.roling@hey.com>
2024-08-26 11:58:08 +0930

    chore: savepoint

--- nixos/laptop.nix
+++ nixos/laptop.nix
@@ -32,6 +32,23 @@
   ];
   # services.fprintd.tod.enable = true;
   # services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix;
+
+  # Intel Accelerated Video Playback
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
+  };
+
+  hardware.opengl = { # hardware.graphics on unstable
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      libvdpau-va-gl
+    ];
+  };
+
+  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; }; # Force intel-media-driver
   
   networking.hostName = "nixos";
 }
