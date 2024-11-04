b1d01183e902e00ec30281a11ccb026e92bb8e35
Jacob <jacob.roling@hey.com>
2024-11-05 01:50:43 +1030

    chore: savepoint

--- nixos/common.nix
+++ nixos/common.nix
@@ -40,6 +40,7 @@
 
     config = {
       allowUnfree = true;
+      allowBroken = true;
     };
   };
 
@@ -70,8 +71,6 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  chaotic.nyx.overlay.onTopOf = "user-pkgs";
 
   # Kernel
   boot.kernelPackages = pkgs.linuxPackages_cachyos;
