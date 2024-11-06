b8a3a09a8a2710d54a078ec49e9521681d4edac1
Jacob <jacob.roling@hey.com>
2024-11-07 00:17:15 +1030

    chore: savepoint

--- modules/nixos/nvidia.nix
+++ modules/nixos/nvidia.nix
@@ -4,10 +4,6 @@
     libsForQt5.qt5.qtwayland
     libsForQt5.qt5ct
     libva
-    # vulkan-loader
-    # (mesa.override {
-    #   enableNvk = true;
-    # })
     vulkan-tools
   ];
   
@@ -29,27 +25,26 @@
   # boot.initrd.kernelModules = [ "nvidia" ];
   # boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
 
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = true;
-    powerManagement.finegrained = false;
-    open = false;
-    nvidiaSettings = true;
-    forceFullCompositionPipeline = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
+  # hardware.nvidia = {
+  #   modesetting.enable = true;
+  #   powerManagement.enable = true;
+  #   powerManagement.finegrained = false;
+  #   open = false;
+  #   nvidiaSettings = true;
+  #   forceFullCompositionPipeline = true;
+  #   package = config.boot.kernelPackages.nvidiaPackages.stable;
+  # };
 
-  boot.kernelModules = [
-    "nvidia"
-    "nvidia_modeset"
-    "nvidia_uvm"
-    "nvidia_drm"
-  ];
+  # boot.kernelModules = [
+  #   "nvidia"
+  #   "nvidia_modeset"
+  #   "nvidia_uvm"
+  #   "nvidia_drm"
+  # ];
 
-  boot.kernelParams = [
-    "nvidia-drm.modeset=1"
-    "nvidia-drm.fbdev=1"
-    "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
-    # "nvidia.NVreg_RegistryDwords=\"PowerMizerEnable=0x1; PerfLevelSrc=0x2222; PowerMizerLevel=0x3; PowerMizerDefault=0x3; PowerMizerDefaultAC=0x3\""
-  ];
+  # boot.kernelParams = [
+  #   "nvidia-drm.modeset=1"
+  #   "nvidia-drm.fbdev=1"
+  #   "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
+  # ];
 }
