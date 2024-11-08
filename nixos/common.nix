c88ca2bbc2b8651729066e99def88038d364f3e5
Jacob <jacob.roling@hey.com>
2024-11-09 01:34:31 +1030

    chore: savepoint

--- nixos/common.nix
+++ nixos/common.nix
@@ -65,6 +65,7 @@
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
       ];
+      download-buffer-size = 100000000;
     };
   };
 
