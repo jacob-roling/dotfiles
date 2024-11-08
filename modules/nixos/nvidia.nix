6e5808ee5d45c8d3fe5f8295b60ae141ce814f78
Jacob <jacob.roling@hey.com>
2024-11-09 10:03:54 +1030

    chore: savepoint

--- modules/nixos/nvidia.nix
+++ modules/nixos/nvidia.nix
@@ -12,48 +12,49 @@
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
-    extraPackages = with pkgs; [
-      # libGL
-      mesa
-      vulkan-loader
-    ];
+    # extraPackages = with pkgs; [
+    #   # libGL
+    #   mesa
+    #   vulkan-loader
+    # ];
   };
 
 
-  environment.sessionVariables = {
-    LD_LIBRARY_PATH = [ "/run/opengl-driver/lib" ];
-    VK_ICD_FILENAMES = "/run/opengl-driver/share/vulkan/icd.d/nouveau_icd.x86_64.json";
-  };
+  # environment.sessionVariables = {
+  #   LD_LIBRARY_PATH = [ "/run/opengl-driver/lib" ];
+  #   VK_ICD_FILENAMES = "/run/opengl-driver/share/vulkan/icd.d/nouveau_icd.x86_64.json";
+  # };
 
   services.xserver = {
     enable = true;
-    videoDrivers = [ "modesetting" ];
+    # videoDrivers = [ "modesetting" ];
+    videoDrivers = [ "nvidia" ];
   };
 
   # If you encounter the problem of booting to text mode you might try adding the Nvidia kernel module manually with:
   # boot.initrd.kernelModules = [ "nvidia" ];
   # boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
 
-  # hardware.nvidia = {
-  #   modesetting.enable = true;
-  #   powerManagement.enable = true;
-  #   powerManagement.finegrained = false;
-  #   open = false;
-  #   nvidiaSettings = true;
-  #   forceFullCompositionPipeline = true;
-  #   package = config.boot.kernelPackages.nvidiaPackages.stable;
-  # };
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = true;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    forceFullCompositionPipeline = true;
+    package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
+  };
 
-  # boot.kernelModules = [
-  #   "nvidia"
-  #   "nvidia_modeset"
-  #   "nvidia_uvm"
-  #   "nvidia_drm"
-  # ];
+  boot.kernelModules = [
+    "nvidia"
+    "nvidia_modeset"
+    "nvidia_uvm"
+    "nvidia_drm"
+  ];
 
-  # boot.kernelParams = [
-  #   "nvidia-drm.modeset=1"
-  #   "nvidia-drm.fbdev=1"
-  #   "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
-  # ];
+  boot.kernelParams = [
+    "nvidia-drm.modeset=1"
+    "nvidia-drm.fbdev=1"
+    "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
+  ];
 }
