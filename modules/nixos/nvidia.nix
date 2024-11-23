05f1239baf3df22f0173f74e51cecd3c697e23ac
Jacob <jacob.roling@hey.com>
2024-11-23 14:55:40 +1030

    chore: savepoint

--- modules/nixos/nvidia.nix
+++ modules/nixos/nvidia.nix
@@ -12,11 +12,11 @@
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
-    # extraPackages = with pkgs; [
-    #   # libGL
-    #   mesa
-    #   vulkan-loader
-    # ];
+    extraPackages = with pkgs; [
+      # libGL
+      vulkan-loader
+      mesa-vulkan-drivers
+    ];
   };
 
 
@@ -27,34 +27,34 @@
 
   services.xserver = {
     enable = true;
-    # videoDrivers = [ "modesetting" ];
-    videoDrivers = [ "nvidia" ];
+    videoDrivers = [ "modesetting" ];
+    # videoDrivers = [ "nvidia" ];
   };
 
   # If you encounter the problem of booting to text mode you might try adding the Nvidia kernel module manually with:
   # boot.initrd.kernelModules = [ "nvidia" ];
   # boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
 
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = true;
-    powerManagement.finegrained = false;
-    open = false;
-    nvidiaSettings = true;
-    forceFullCompositionPipeline = true;
-    package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
-  };
+  # hardware.nvidia = {
+  #   modesetting.enable = true;
+  #   powerManagement.enable = true;
+  #   powerManagement.finegrained = false;
+  #   open = false;
+  #   nvidiaSettings = true;
+  #   forceFullCompositionPipeline = true;
+  #   package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
+  # };
 
-  boot.kernelModules = [
-    "nvidia"
-    "nvidia_modeset"
-    "nvidia_uvm"
-    "nvidia_drm"
-  ];
+  # boot.kernelModules = [
+  #   "nvidia"
+  #   "nvidia_modeset"
+  #   "nvidia_uvm"
+  #   "nvidia_drm"
+  # ];
 
-  boot.kernelParams = [
-    "nvidia-drm.modeset=1"
-    "nvidia-drm.fbdev=1"
-    "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
-  ];
+  # boot.kernelParams = [
+  #   "nvidia-drm.modeset=1"
+  #   "nvidia-drm.fbdev=1"
+  #   "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
+  # ];
 }
