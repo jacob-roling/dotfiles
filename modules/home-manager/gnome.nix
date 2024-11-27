57439cd52f888d2d9096542e8b9eff2bf9ba01ba
Jacob <jacob.roling@hey.com>
2024-11-28 02:19:37 +1030

    chore: savepoint

--- modules/home-manager/gnome.nix
+++ modules/home-manager/gnome.nix
@@ -3,6 +3,20 @@
   dconf = {
     enable = true;
     settings = {
+      "org/gnome/shell" = {
+        disable-user-extensions = false; # enables user extensions
+        enabled-extensions = with gnomeExtensions; [
+          appindicator.extensionUuid
+          pano.extensionUuid
+          quick-settings-tweaker.extensionUuid
+          arc-menu.extensionUuid
+          wiggle.extensionUuid
+          blur-my-shell.extensionUuid
+          caffeine.extensionUuid
+          auto-activities.extensionUuid
+        ];
+      };
+
       "org/gnome/desktop/background".picture-uri = "/home/${settings.username}/Pictures/wallpaper.jpg";
       "org/gnome/desktop/interface".color-scheme = "prefer-dark";
     };
