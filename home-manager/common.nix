9456ffd245b472ac363d7ed7ebbe0dade36b904f
Jacob <jacob.roling@hey.com>
2024-12-08 15:05:28 +1030

    chore: savepoint

--- home-manager/common.nix
+++ home-manager/common.nix
@@ -9,7 +9,7 @@
 }: {
   imports = [
     inputs.nix-colors.homeManagerModules.default
-    inputs.nur.hmModules.nur
+    inputs.nur.modules.home-manager.default
     # inputs.chaotic.homeManagerModules.default
     # inputs.nixvim.homeManagerModules.nixvim
     # Custom Home Manager Modules
