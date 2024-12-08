684c06df8a70078a4ca9c8e6b9b1c042c159bab3
Jacob <jacob.roling@hey.com>
2024-12-08 15:49:23 +1030

    chore: savepoint

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -1,5 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 {
+  programs.hyprland.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    networkmanagerapplet
+  ];
+
   services = {
     displayManager = {
       sddm = {
@@ -9,23 +15,23 @@
     };
   };
 
-  security.polkit = {
-    enable = true;
-    extraConfig = ''
-      polkit.addRule(function(action, subject) {
-        if (
-          subject.isInGroup("users")
-            && (
-              action.id == "org.freedesktop.login1.reboot" ||
-              action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
-              action.id == "org.freedesktop.login1.power-off" ||
-              action.id == "org.freedesktop.login1.power-off-multiple-sessions"
-            )
-          )
-        {
-          return polkit.Result.YES;
-        }
-      });
-    '';
-  };
+  # security.polkit = {
+  #   enable = true;
+  #   extraConfig = ''
+  #     polkit.addRule(function(action, subject) {
+  #       if (
+  #         subject.isInGroup("users")
+  #           && (
+  #             action.id == "org.freedesktop.login1.reboot" ||
+  #             action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
+  #             action.id == "org.freedesktop.login1.power-off" ||
+  #             action.id == "org.freedesktop.login1.power-off-multiple-sessions"
+  #           )
+  #         )
+  #       {
+  #         return polkit.Result.YES;
+  #       }
+  #     });
+  #   '';
+  # };
 }
