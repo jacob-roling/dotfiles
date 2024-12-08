f6249c3ce0af67521781412e1cc25fd9854c43b0
Jacob <jacob.roling@hey.com>
2024-12-08 13:56:40 +1030

    chore: savepoint

--- home-manager/common.nix
+++ home-manager/common.nix
@@ -58,6 +58,8 @@
   # Allow Home Manager to manage itself
   programs.home-manager.enable = true;
 
+  home-manager.backupFileExtension = 'backup';
+
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
