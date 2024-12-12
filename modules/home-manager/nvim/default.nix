8a822a759ea0e285ac14dc1cc615c1b84fd4bb86
Jacob <jacob.roling@hey.com>
2024-12-13 02:04:02 +1030

    chore: savepoint

--- modules/home-manager/nvim/default.nix
+++ modules/home-manager/nvim/default.nix
@@ -12,10 +12,71 @@
 
   programs.nixvim = {
     enable = true;
+    
     opts = {
       number = true;
       relativenumber = true;
       shiftwidth = 2;
+      mouse = "a";
+      showmode = false;
+
+      clipboard = {
+        providers = {
+          wl-copy.enable = true; # For Wayland
+          xsel.enable = true; # For X11
+        };
+
+        # Remove this option if you want your OS clipboard to remain independent.
+        register = "unnamedplus";
+      };
+
+      breakindent = true;
+
+      # Save undo history
+      undofile = true;
+
+      # Case-insensitive searching UNLESS \C or one or more capital letters in search term
+      ignorecase = true;
+      smartcase = true;
+
+      # Keep signcolumn on by default
+      signcolumn = "yes";
+
+      # Decrease update time
+      updatetime = 250;
+
+      # Decrease mapped sequence wait time
+      # Displays which-key popup sooner
+      timeoutlen = 300;
+
+      # Configure how new splits should be opened
+      splitright = true;
+      splitbelow = true;
+
+      # Sets how neovim will display certain whitespace characters in the editor
+      #  See `:help 'list'`
+      #  See `:help 'listchars'`
+      list = true;
+      # NOTE: .__raw here means that this field is raw lua code
+      listchars.__raw = "{ tab = '» ', trail = '·', nbsp = '␣' }";
+
+      # Preview subsitutions live, as you type!
+      inccommand = "split";
+
+      # Show which line your cursor is on
+      cursorline = true;
+
+      # Minimal number of screen lines to keep above and below the cursor
+      scrolloff = 10;
+
+      # See `:help hlsearch`
+      hlsearch = true;
+    };
+
+    globals = {
+      mapleader = " ";
+      maplocalleader = " ";
+      have_nerd_font = true;
     };
   };
 }
