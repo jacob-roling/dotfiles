32db31886548c8c1b4ffc42e38818b7164937ed5
Jacob <jacob.roling@hey.com>
2024-12-14 01:49:35 +1030

    chore: savepoint

--- modules/home-manager/vscode.nix
+++ modules/home-manager/vscode.nix
@@ -55,7 +55,7 @@ in
       #   "editor.defaultFormatter" = "esbenp.prettier-vscode";
       # };
     };
-    extensions = with extensionsList.vscode-marketplace; [
+    extensions = with extensionsList.vscode-marketplace-release; [
       bbenoist.nix
       tamasfe.even-better-toml
       astro-build.astro-vscode
