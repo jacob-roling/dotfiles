4a79802de28554d778d7201350de7def42cedb85
Jacob <jacob.roling@hey.com>
2024-12-19 01:42:35 +1030

    chore: savepoint

--- modules/home-manager/kitty.nix
+++ modules/home-manager/kitty.nix
@@ -8,9 +8,9 @@
       enable_audio_bell = "no";
     };
 
-    keybindings = {
-      "kitty_mod+enter" = "launch --cwd=current";
-      "kitty_mod+q" = "close_window";
-    };
+    # keybindings = {
+    #   "kitty_mod+enter" = "launch --cwd=current";
+    #   "kitty_mod+q" = "close_window";
+    # };
   };
 }
