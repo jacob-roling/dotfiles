c0f8ccb9a66a6d981bb29ceb629b2cb95cbbccaf
Jacob <jacob.roling@hey.com>
2024-12-19 01:20:13 +1030

    chore: savepoint

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -8,9 +8,21 @@
 
   programs.tmux = {
     enable = true;
+    mouse = true;
+    baseIndex = 1;
+    sensibleOnTop = true;
+    disableConfirmationPrompt = true;
+
+
     plugins = with pkgs.tmuxPlugins; [
       sensible
       vim-tmux-navigator
+      yank
     ];
+
+    extraConfig = ''
+      bind '"' split-window -v -c "{#pane_current_path}"
+      bind % split-window -h -c "{#pane_current_path}"
+    '';
   };
 }
