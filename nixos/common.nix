90d8e1db0974f57560a3896578d75a3c5e49c760
Jacob <jacob.roling@hey.com>
2024-12-19 01:49:44 +1030

    chore: savepoint

--- nixos/common.nix
+++ nixos/common.nix
@@ -199,6 +199,12 @@
     wpa_supplicant_gui
   ];
 
+  # Keyboard
+  services.xserver = {
+    layout = "us";
+    xkbOptions = "ctrl:nocaps";
+  };
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
